3d6c246c9e93041ab8a4a9c71387de0055b9a359
Divya Goel <70468466+DivyaGoel@users.noreply.github.com>
2022-06-06 14:54:39 +0530

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function createNotes(noteitem)
+{
+    return <Note key = {noteitem.key} title = {noteitem.content} content = {noteitem.content} />;
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(createNotes)};
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
